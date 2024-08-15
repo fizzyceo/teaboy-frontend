2bcd7f5b5cc7e3e743c7a988e97542b215a255b9
MoncefME <ka_moussaoui@esi.dz>
2024-08-16 00:51:05 +0100

    build before push !!

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -27,8 +27,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        xl: "h-12 w-full py-6 text-xl",
-        totalDisplay: "h-12 w-2/3 py-2",
+        icon: "h-10 w-10",
       },
       iconPlacement: {
         left: "flex gap-4",
